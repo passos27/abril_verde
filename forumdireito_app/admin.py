306014cf827e1590b99f57cde5ab6e65cccd015f
Francisco Passos <passos.27@hotmail.com>
2018-05-16 12:32:05 -0500

    commit inicial

---
+++ forumdireito_app/admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+
+from forumdireito_app.models import *
+
+
+
+
+# Register your models here.
+class Palestrassadmin(admin.ModelAdmin):
+    list_display = ['titulo', ]
+    search_fields = ['palestras__titulo',]
+
+class Palestranteadmin(admin.ModelAdmin):
+    list_display = ['nome', ]
+    search_fields = ['palestrante__nome', ]
+
+admin.site.register(Palestras, Palestrassadmin)
+admin.site.register(Palestrante, Palestranteadmin)
---
+++ forumdireito_app/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+
+class Palestrante(models.Model):
+    nome = models.CharField(u'Título', max_length=100, null=True, blank=True)
+    foto = models.ImageField(upload_to='foto_palestrante', null=True, blank=True)
+    curriculo = models.TextField(u'Mini currículo', max_length=140, null=True, blank=True)
+    dt_abertura = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    dt_atualizacao = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = u'Palestrante'
+        verbose_name_plural = u'Palestrantes'
+    def __unicode__(self):
+        return u'%s' % self.nome
+
+
+class Palestras(models.Model):
+    palestrante = models.ForeignKey(Palestrante,on_delete=models.CASCADE, blank=True, null=True)
+    titulo = models.CharField(u'Título', max_length=100, null=True, blank=True)
+    descricao = models.TextField(u'Descreva a palestra', max_length=140, null=True, blank=True)
+    dt_abertura = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    dt_atualizacao = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = u'Palestra'
+        verbose_name_plural = u'Palestras'
+    def __unicode__(self):
+        return u'%s' % self.titulo
